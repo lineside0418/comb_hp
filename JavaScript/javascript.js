032584eb8a8f24e78f6eefbf42bc54645d61a0df
tennengas <184702327+tennengas@users.noreply.github.com>
2024-10-13 13:11:13 +0900

    transformクソ仕様

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,1 +1,8 @@
-
+$(function(){
+    $("#hamburger_menu").on("click",function(){
+        $("#drop_down_menu").slideToggle();
+        $("#middle_bar").toggleClass("middle_bar_move");
+        $("#high_bar").toggleClass("high_bar_move");
+        $("#low_bar").toggleClass("low_bar_move");
+    })
+});
